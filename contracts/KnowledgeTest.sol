8faa28bb2b07c54da2d5a6c84d851378f52831ed
Douglas Calhoun <douglascalhoun@Douglass-MacBook-Air.local>
2022-09-20 10:59:22 -0700

    KnowledgeTest Complete

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,38 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    modifier onlyOwner(){
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        tokens[0] = "VET";
+    }
+
+    function concatenate(string calldata a, string calldata b) public pure returns (string memory) {
+        return string.concat(a, b);
+    }
+
+    function getBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function transferAll(address to) public onlyOwner {
+        to.call{value: address(this).balance}("");
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    receive() external payable {
+
     }
 }
